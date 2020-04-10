e60ac9b11025706615ef2214cd1d950a63c3ed7f
Joe Coburn <coburnj11@gmail.com>
2020-04-10 23:03:51 +0100

    Bugfix logging level for all handlers

--- Pi_Car/app.py
+++ Pi_Car/app.py
@@ -15,11 +15,11 @@ def create_app(config_file="config/local_config.py"):
         maxBytes=10000000,
         backupCount=4,
     )
-    log_file_handler.setLevel(app.config.get("LOGGER_LEVEL", "ERROR"))
     log_file_handler.setFormatter(
         logging.Formatter("[%(asctime)s] %(levelname)s in %(module)s: %(message)s")
     )
     app.logger.addHandler(log_file_handler)
+    app.logger.setLevel(app.config.get("LOGGER_LEVEL", "ERROR"))
 
     app.logger.info("----- STARTING APP ------")
     app.register_blueprint(data_blueprint)
