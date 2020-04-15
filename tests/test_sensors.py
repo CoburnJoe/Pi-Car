b401bfdcc4649705936c03ee9c2c4199a680b2eb
Joe Coburn <coburnj11@gmail.com>
2020-04-15 19:43:22 +0100

    First unit tests for sensors

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -1,9 +1,29 @@
-import pytest
 from unittest.mock import patch
 from Pi_Car.sensors import Sensors
+from gpiozero import exc
 
 
 class TestSensors:
-    @patch("gpiozero.Button.is_pressed")
-    def test_get_boot_status_positive(self, mock_is_pressed):
-        assert 1 == 1
+    @patch("Pi_Car.sensors.Button")
+    def test_get_boot_status_bad_pin_factory(self, mock_button):
+        mock_button.side_effect = exc.BadPinFactory
+        result = Sensors.get_boot_status()
+        assert result == "Unknown"
+
+    @patch("Pi_Car.sensors.Button")
+    def test_get_boot_status_other_pin_error(self, mock_button):
+        mock_button.side_effect = TypeError
+        result = Sensors.get_boot_status()
+        assert result == "Unknown"
+
+    @patch("Pi_Car.sensors.Button")
+    def test_get_boot_status_closed(self, mock_button):
+        mock_button.return_value = type("Button", (), {"is_pressed": True})
+        result = Sensors.get_boot_status()
+        assert result == "Closed"
+
+    @patch("Pi_Car.sensors.Button")
+    def test_get_boot_status_open(self, mock_button):
+        mock_button.return_value = type("Button", (), {"is_pressed": False})
+        result = Sensors.get_boot_status()
+        assert result == "Open"
