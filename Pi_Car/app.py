ffd59d78a186eebf528a629dfcde9d821bcf8961
Joe Coburn <coburnj11@gmail.com>
2020-04-22 11:24:42 +0100

    WIP very rough first pass at camera stream

--- Pi_Car/app.py
+++ Pi_Car/app.py
@@ -1,8 +1,17 @@
 import logging
 
-from flask import Flask
+from flask import Flask, render_template, Response
 from logging.handlers import RotatingFileHandler
 from .data import data_blueprint
+
+try:
+    from picamera import PiCamera
+except ImportError:
+    PiCamera = None
+
+
+import socket
+import io
 
 
 def create_app(config_file="config/local_config.py"):
@@ -26,5 +35,33 @@ def create_app(config_file="config/local_config.py"):
     app.logger.info("----- STARTING APP ------")
     app.register_blueprint(data_blueprint)
     app.logger.info("----- FINISHED STARTING APP -----")
+
+    if PiCamera:
+        camera = PiCamera()
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+    else:
+        camera = None
+
+    @app.route("/camera/")
+    def index():
+        """Video streaming ."""
+        return render_template("camera.html")
+
+    def gen():
+        """Video streaming generator function."""
+        while True:
+            camera.capture("pic.jpg")
+            yield (
+                b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n"
+                + open("pic.jpg", "rb").read()
+                + b"\r\n"
+            )
+
+    @app.route("/video_feed")
+    def video_feed():
+        """Video streaming route. Put this in the src attribute of an img tag."""
+        return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
     return app
