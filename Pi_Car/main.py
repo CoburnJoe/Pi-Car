fd239e74e06911631ee4e2539bd743f96bfaad48
Joe Coburn <coburnj11@gmail.com>
2020-04-25 16:49:42 +0100

    UI changes for Pi display

--- Pi_Car/main.py
+++ Pi_Car/main.py
@@ -7,14 +7,14 @@ data_blueprint = Blueprint("data", __name__)
 
 @data_blueprint.route("/")
 def show():
-    # app.logger.info("Starting to retrieve core data")
-    # temperature = Sensors.get_external_temp()
-    # boot_status = Sensors.get_boot_status()
-    # light_status = Sensors.get_light_status()
-    # reverse_light = Sensors.get_reverse_status()
-    # fog_light = Sensors.get_fog_light_status()
-    # rear_distance = Sensors.get_rear_distance_sensor()
-    #
+    app.logger.info("Starting to retrieve core data")
+    temperature = Sensors.get_external_temp()
+    boot_status = Sensors.get_boot_status()
+    light_status = Sensors.get_light_status()
+    reverse_light = Sensors.get_reverse_status()
+    fog_light = Sensors.get_fog_light_status()
+    rear_distance = Sensors.get_rear_distance_sensor()
+
     result = {
         "temperature": "temperature",
         "boot": "boot_status",
@@ -23,9 +23,9 @@ def show():
         "rear_distance": "rear_distance",
         "fog": "fog_light",
     }
-    #
-    # Sensors.beep()
-    # app.logger.info("Finished retrieving core data")
-    # app.logger.debug(f"Core data: {result}")
+
+    Sensors.beep()
+    app.logger.info("Finished retrieving core data")
+    app.logger.debug(f"Core data: {result}")
     # return jsonify(result)
-    return render_template("main.html", data=result)
+    return render_template("main.html")
