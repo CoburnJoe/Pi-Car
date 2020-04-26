e89b86f28446db2f05b56109121695fa1f33effe
Joe Coburn <coburnj11@gmail.com>
2020-04-26 20:35:00 +0100

    Added redirect from reverse to main

--- Pi_Car/reverse.py
+++ Pi_Car/reverse.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask import current_app as app
+from .sensors import Sensors
 
 reverse_blueprint = Blueprint("reverse", __name__)
 
@@ -13,3 +14,19 @@ def reverse():
     app.logger.debug(f"Camera streaming url: {stream_url}")
     app.logger.info("Finished loading reverse route")
     return render_template(template_name_or_list="reverse.html", base_url=stream_url)
+
+
+@reverse_blueprint.route("/reverse/beep/")
+def reverse_beep():
+    app.logger.info("Starting to load reverse rear detection")
+    sensors = Sensors()
+    reverse_light = sensors.get_reverse_status()
+    if reverse_light:
+        return "Exit"
+
+    rear_distance_item = sensors.get_rear_distance_sensor()
+    if rear_distance_item:
+        sensors.beep()
+    app.logger.info("Finished loading reverse rear detection")
+
+    return "Success"
